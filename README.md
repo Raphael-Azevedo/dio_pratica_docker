0ce114af89bbc8cf977e034dfa91ea17bc2755d1
rlzs <rlzs@gft.com>
2022-11-17 11:22:55 -0300

    docker pratica v1.0

---
+++ README.md
@@ -0,0 +1,1 @@
+Pratica 3 do bootcamp de linux utilizando um cenario de docker 
---
+++ banco.sql
@@ -0,0 +1,12 @@
+CREATE TABLE dados (
+    AlunoID int,
+    Nome varchar(50),
+    Sobrenome varchar(50),
+    Idade varchar(50),
+    Altura varchar(50),
+    Nacionalidade varchar(50),
+    Tipo Sanguínio verchar(50),
+    Endereco varchar(150),
+    Cidade varchar(50),	
+    Host varchar(50)
+);
